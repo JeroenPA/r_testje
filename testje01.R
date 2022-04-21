9e39fadf7ac6cc70e63cd885da736a00e39b5aeb
J.P.A. Schreurs <J.P.A.Schreurs@rug.nl>
2022-04-21 13:53:22 +0200

    added library calls&changed variable names

--- testje01.R
+++ testje01.R
@@ -1,5 +1,7 @@
 #testje 3
 
-bs <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1dIMUG_lrMwREnAMmwlh-f_DRdMrcSed5_Blb4wuBxg4/edit#gid=1712669767", "Inventory", NULL, TRUE, FALSE)
 
-bs
+selection_data <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1dIMUG_lrMwREnAMmwlh-f_DRdMrcSed5_Blb4wuBxg4/edit#gid=1712669767", "Inventory", NULL, TRUE, FALSE)
+
+selection_data
+
