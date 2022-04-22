92617fa0ff26923dbac7103d9f7782ecad581541
J.P.A. Schreurs <J.P.A.Schreurs@rug.nl>
2022-04-22 13:55:09 +0200

    Load Google sheet

--- testje01.R
+++ testje01.R
@@ -1,7 +1,9 @@
 #testje 3
+library(googlesheets4, tibble)
 
 
-selection_data <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1dIMUG_lrMwREnAMmwlh-f_DRdMrcSed5_Blb4wuBxg4/edit#gid=1712669767", "Inventory", NULL, TRUE, FALSE)
+
+selection_data <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1bTd4XG18IZOreCjc_VZhB75t1roBPbS8k_-w49Srxlw/edit#gid=1712669767")
 
 selection_data
 
