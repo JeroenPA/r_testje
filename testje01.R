12a468b9767e44fabc07fd3b0621cfc0c54c5d59
J.P.A. Schreurs <J.P.A.Schreurs@rug.nl>
2022-05-10 10:29:56 +0200

    Added print

--- testje01.R
+++ testje01.R
@@ -2,8 +2,10 @@
 library(googlesheets4, tibble)
 
 
-
 selection_data <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1bTd4XG18IZOreCjc_VZhB75t1roBPbS8k_-w49Srxlw/edit#gid=1712669767")
 
-selection_data
+print(selection_data)
+
+
+
 
